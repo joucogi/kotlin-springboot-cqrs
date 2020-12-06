a2622e6e3d08260cfbc3f9413aea5a30022cc6d8
Joel Coll Costa <joel.coll@gmail.com>
2020-12-06 13:48:56 +0100

    Health Check controller

---
+++ apps/main/kotlin/com/prameprimo/apps/shop/controller/health_check/HealthCheckGetController.kt
@@ -0,0 +1,18 @@
+package com.prameprimo.apps.shop.controller.health_check
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckGetController {
+
+    @GetMapping("/health-check")
+    fun index(): ResponseEntity<HashMap<String, String>> =
+            ResponseEntity.ok().body(
+                    hashMapOf(
+                            "status" to "ok",
+                            "application" to "shop"
+                    )
+            )
+}
