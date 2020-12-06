adfc026a7b94e80a63c79743d53b71662d789932
Joel Coll Costa <joel.coll@gmail.com>
2020-12-06 14:26:54 +0100

    Starter run application depending arguments

--- apps/main/kotlin/com/prameprimo/apps/Starter.kt
+++ apps/main/kotlin/com/prameprimo/apps/Starter.kt
@@ -1,7 +1,10 @@
 package com.prameprimo.apps
 
+import com.prameprimo.apps.backoffice.BackofficeApplication
 import com.prameprimo.apps.shop.ShopApplication
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.runApplication
+import kotlin.reflect.KClass
 
 fun main(args: Array<String>) {
     val starter = Starter()
@@ -9,7 +12,33 @@ fun main(args: Array<String>) {
 }
 
 class Starter {
+    private val applications = hashMapOf<String, KClass<*>>(
+            "shop" to ShopApplication::class,
+            "backoffice" to BackofficeApplication::class
+    )
+
     fun run(args: Array<String>) {
-        runApplication<ShopApplication>(*args)
+        ensureThereAreEnoughArguments(args)
+
+        val applicationName = args[0]
+
+        ensureApplicationExists(applicationName)
+
+        val applicationClass: KClass<*>? = applications[applicationName]
+        val application = SpringApplication(applicationClass?.java)
+
+        application.run(*args)
+    }
+
+    private fun ensureApplicationExists(applicationName: String) {
+        if (!applications.containsKey(applicationName)) {
+            throw RuntimeException("Application $applicationName does not exist")
+        }
+    }
+
+    private fun ensureThereAreEnoughArguments(args: Array<String>) {
+        if (args.isEmpty()) {
+            throw RuntimeException("There are not enough arguments")
+        }
     }
 }
