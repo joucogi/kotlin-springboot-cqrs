10866d3d0a94bbfdf0e0298e7fda59145ef39076
Joel Coll Costa <joel.coll@gmail.com>
2020-12-06 15:27:39 +0100

    Add environment variables to set applications ports

--- apps/main/kotlin/com/prameprimo/apps/backoffice/config/BackofficePortConfiguration.kt
+++ apps/main/kotlin/com/prameprimo/apps/backoffice/config/BackofficePortConfiguration.kt
@@ -1,5 +1,6 @@
 package com.prameprimo.apps.backoffice.config
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.web.server.ConfigurableWebServerFactory
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.stereotype.Component
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component
 @Component
 class BackofficePortConfiguration : WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     override fun customize(factory: ConfigurableWebServerFactory?) {
-        factory!!.setPort(8081)
+        val params = Dotenv.load()
+        factory!!.setPort(params["BACKOFFICE_SERVER_PORT"].toInt())
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	implementation("io.github.cdimascio:dotenv-kotlin:6.2.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
