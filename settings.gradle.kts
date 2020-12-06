4cdb4951cc1066431548815eaf4582b4896a7570
Joel Coll Costa <joel.coll@gmail.com>
2020-12-06 18:57:03 +0100

    Configure gradle for multiproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "marketplace"
+
+include(":shop")
+project(":shop").projectDir = File("src/shop")
+
+include(":backoffice")
+project(":backoffice").projectDir = File("src/backoffice")
+
+include(":shared")
+project(":shared").projectDir = File("src/shared")
