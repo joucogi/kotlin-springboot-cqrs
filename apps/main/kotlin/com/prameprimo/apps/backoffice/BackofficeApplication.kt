eaec770e83611ea5c40ae90531ec084f0f042cd3
Joel Coll Costa <joel.coll@gmail.com>
2020-12-18 19:57:32 +0100

    Make Starter multiproject

--- apps/main/kotlin/com/prameprimo/apps/backoffice/BackofficeApplication.kt
+++ apps/main/kotlin/com/prameprimo/apps/backoffice/BackofficeApplication.kt
@@ -1,6 +1,10 @@
 package com.prameprimo.apps.backoffice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(
+        basePackages = ["com.prameprimo.backoffice", "com.prameprimo.shared", "com.prameprimo.apps.backoffice"]
+)
 class BackofficeApplication
