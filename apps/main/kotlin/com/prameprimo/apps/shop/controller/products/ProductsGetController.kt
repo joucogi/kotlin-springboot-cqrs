7d282ea86e954fcbc591d24c3132f7d96c72bc0f
Joel Coll Costa <joel.coll@gmail.com>
2020-12-19 14:35:21 +0100

    ProductsGetController.kt using CQRS

--- apps/main/kotlin/com/prameprimo/apps/shop/controller/products/ProductsGetController.kt
+++ apps/main/kotlin/com/prameprimo/apps/shop/controller/products/ProductsGetController.kt
@@ -1,7 +1,7 @@
 package com.prameprimo.apps.shop.controller.products
 
 import com.prameprimo.shared.infrastructure.ApiController
-import com.prameprimo.shop.application.search_all.AllProductsSearcherQuery
+import com.prameprimo.shop.application.products.search_all.AllProductsSearcherQuery
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
