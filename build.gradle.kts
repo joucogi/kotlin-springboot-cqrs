dddc6dd9e57f4097d0d2f9e64c5400644a6a9494
Joel Coll Costa <joel.coll@gmail.com>
2020-12-20 18:19:38 +0100

    Unit test for all products in shop application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,113 +1,142 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("java")
-	id("org.springframework.boot") version "2.4.0"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.10"
-	kotlin("plugin.spring") version "1.4.10"
+    application
+    id("java")
+    id("org.springframework.boot") version "2.4.0"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
 }
 
 allprojects {
 
-	apply {
-		plugin("java")
-		plugin("org.jetbrains.kotlin.jvm")
-		plugin("io.spring.dependency-management")
-		plugin("org.springframework.boot")
-	}
+    apply {
+        plugin("java")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("io.spring.dependency-management")
+        plugin("org.springframework.boot")
+    }
 
-	version = "0.0.1"
-	java.sourceCompatibility = JavaVersion.VERSION_11
-	java.targetCompatibility = JavaVersion.VERSION_11
+    version = "0.0.1"
+    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.targetCompatibility = JavaVersion.VERSION_11
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 
-	dependencies {
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-		implementation("org.reflections:reflections:0.9.11")
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("org.reflections:reflections:0.9.11")
 
-		implementation("io.github.cdimascio:dotenv-kotlin:6.2.1")
-	}
+        implementation("io.github.cdimascio:dotenv-kotlin:6.2.1")
+
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+
+        testLogging {
+            events = mutableSetOf(
+                    PASSED,
+                    FAILED,
+                    SKIPPED
+            )
+        }
+    }
 }
 
 // All subprojects
 subprojects {
-	group = "com.prameprimo.${rootProject.name}"
+    group = "com.prameprimo.${rootProject.name}"
 
-	sourceSets.main {
-		java.srcDirs("main/kotlin")
-		resources.srcDirs("main/resources")
-	}
+    sourceSets {
+        main {
+            java.srcDirs("main/kotlin")
+            resources.srcDirs("main/resources")
+        }
 
-	sourceSets.test {
-		java.srcDirs("test/kotlin")
-		resources.srcDirs("test/resources")
-	}
+        test {
+            java.srcDirs("test/kotlin")
+            resources.srcDirs("test/resources")
+        }
+    }
 
-	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter-web")
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
 
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation(rootProject.sourceSets["main"].output)
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation(rootProject.sourceSets["main"].output)
 
-		if (project.name != "shared") {
-			implementation(project(":shared"))
+        if (project.name != "shared") {
+            implementation(project(":shared"))
 
-			testImplementation(project(":shared").dependencyProject.sourceSets["test"].output)
-		}
-	}
+            testImplementation(project(":shared").dependencyProject.sourceSets["test"].output)
+        }
+    }
 
-	tasks.withType<BootJar> {
-		enabled = false
-	}
+    tasks.getByName<BootJar>("bootJar") {
+        enabled = false
+    }
 
-	tasks.withType<Jar> {
-		enabled = true
-	}
+    tasks.getByName<Jar>("jar") {
+        enabled = true
+    }
 
+    tasks.withType<JavaCompile> {
+        enabled = false
+    }
 }
 
 buildscript {
-	repositories {
-		mavenCentral()
-	}
-	dependencies {
-		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.4.0")
-	}
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.4.0")
+    }
 }
 
-sourceSets.main {
-	java.srcDirs("apps/main/kotlin")
-	resources.srcDirs("apps/main/resources")
-}
+sourceSets {
+    main {
+        java.srcDirs("apps/main/kotlin")
+        resources.srcDirs("apps/main/resources")
+    }
 
-sourceSets.test {
-	java.srcDirs("apps/test/kotlin")
-	resources.srcDirs("apps/test/resources")
+    test {
+        java.srcDirs("apps/test/kotlin")
+        resources.srcDirs("apps/test/resources")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    enabled = false
+}
+
+application {
+    mainClassName = "com.prameprimo.apps.StarterKt"
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
-	implementation(project(":shared"))
-	implementation(project(":backoffice"))
-	implementation(project(":shop"))
+    implementation(project(":shared"))
+//    implementation(project(":backoffice"))
+    implementation(project(":shop"))
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(project(":shared").dependencyProject.sourceSets["test"].output)
 }
