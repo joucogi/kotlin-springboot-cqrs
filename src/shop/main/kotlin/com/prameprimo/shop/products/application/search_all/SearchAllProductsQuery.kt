2437db5b3787e759dba72f67f5e8e84500bb71fb
Joel Coll Costa <joel.coll@gmail.com>
2020-12-20 20:39:48 +0100

    Rename SearchAllProductsQuery.kt and SearchAllProductsQueryHandler.kt

--- src/shop/main/kotlin/com/prameprimo/shop/products/application/search_all/SearchAllProductsQuery.kt
+++ src/shop/main/kotlin/com/prameprimo/shop/products/application/search_all/SearchAllProductsQuery.kt
@@ -2,4 +2,4 @@ package com.prameprimo.shop.products.application.search_all
 
 import com.prameprimo.shared.domain.bus.query.Query
 
-class AllProductsSearcherQuery: Query<Any?>
+class SearchAllProductsQuery: Query<Any?>
--- src/shop/main/kotlin/com/prameprimo/shop/products/application/search_all/SearchAllProductsQueryHandler.kt
+++ src/shop/main/kotlin/com/prameprimo/shop/products/application/search_all/SearchAllProductsQueryHandler.kt
@@ -2,13 +2,14 @@ package com.prameprimo.shop.products.application.search_all
 
 import com.prameprimo.shared.domain.Service
 import com.prameprimo.shared.domain.bus.query.QueryHandler
+import com.prameprimo.shop.products.application.ProductsResponse
 
 @Service
-class AllProductsSearcherQueryHandler(
+class SearchAllProductsQueryHandler(
         private val searcher: AllProductsSearcher
-) : QueryHandler<AllProductsSearcherQuery> {
+) : QueryHandler<SearchAllProductsQuery, ProductsResponse> {
 
-    override fun handle(query: AllProductsSearcherQuery): HashMap<String, String> {
+    override fun handle(query: SearchAllProductsQuery): ProductsResponse {
         return searcher.search()
     }
 }
