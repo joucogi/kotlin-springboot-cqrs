d1908fc784736789bd8a11b4517d8800adf92c9a
Joel Coll Costa <joel.coll@gmail.com>
2020-12-21 16:33:30 +0100

    Create Shop Hibernate Configuration

--- apps/main/kotlin/com/prameprimo/apps/shop/ShopApplication.kt
+++ apps/main/kotlin/com/prameprimo/apps/shop/ShopApplication.kt
@@ -2,9 +2,10 @@ package com.prameprimo.apps.shop
 
 import com.prameprimo.shared.domain.Service
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [HibernateJpaAutoConfiguration::class])
 @ComponentScan(
         basePackages = ["com.prameprimo.shop", "com.prameprimo.shared", "com.prameprimo.apps.shop"],
         includeFilters = [ComponentScan.Filter(Service::class)]
---
+++ src/shop/main/resources/database/shop.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS products (
+    id       CHAR(36)     NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+    )
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
