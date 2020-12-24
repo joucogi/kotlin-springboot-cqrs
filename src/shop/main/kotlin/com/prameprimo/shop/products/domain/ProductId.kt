d1f4fe0597c8370c4f4e3fa037f93fe147d5aa35
Joel Coll Costa <joel.coll@gmail.com>
2020-12-24 18:13:31 +0100

    Improve product repository

--- src/shop/main/kotlin/com/prameprimo/shop/products/domain/ProductId.kt
+++ src/shop/main/kotlin/com/prameprimo/shop/products/domain/ProductId.kt
@@ -2,4 +2,4 @@ package com.prameprimo.shop.products.domain
 
 import com.prameprimo.shared.domain.Identifier
 
-data class ProductId(override val value: String) : Identifier()
+class ProductId(value: String) : Identifier(value)
