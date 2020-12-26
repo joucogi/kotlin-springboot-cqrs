4681689e4aed0967dd05059cac0c2255ca45c8c4
Joel Coll Costa <joel.coll@gmail.com>
2020-12-26 21:00:14 +0100

    Validate Identifier is UUID

--- src/shared/main/kotlin/com/prameprimo/shared/domain/Identifier.kt
+++ src/shared/main/kotlin/com/prameprimo/shared/domain/Identifier.kt
@@ -1,11 +1,21 @@
 package com.prameprimo.shared.domain
 
 import java.io.Serializable
+import java.util.*
 
-abstract class Identifier(val value: String) : Serializable {
-    init {
-        if (value == "") {
-            throw IllegalArgumentException()
-        }
+abstract class Identifier : Serializable {
+    val value: String
+
+    constructor(value: String) {
+        ensureValidUuid(value)
+        this.value = value
+    }
+
+    constructor() {
+        this.value = ""
+    }
+
+    private fun ensureValidUuid(value: String) {
+        UUID.fromString(value)
     }
 }
--- src/shop/main/kotlin/com/prameprimo/shop/products/domain/ProductId.kt
+++ src/shop/main/kotlin/com/prameprimo/shop/products/domain/ProductId.kt
@@ -2,4 +2,8 @@ package com.prameprimo.shop.products.domain
 
 import com.prameprimo.shared.domain.Identifier
 
-class ProductId(value: String) : Identifier(value)
+class ProductId : Identifier {
+    constructor(value: String): super(value)
+    constructor(): super()
+}
+
