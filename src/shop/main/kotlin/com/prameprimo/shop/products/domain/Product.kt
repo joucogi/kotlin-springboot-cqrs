d94897fb9177d21cdda05612ce7270ef191c6b3c
Joel Coll Costa <joel.coll@gmail.com>
2020-12-26 21:00:14 +0100

    Validate Identifier is UUID

--- src/shop/main/kotlin/com/prameprimo/shop/products/domain/Product.kt
+++ src/shop/main/kotlin/com/prameprimo/shop/products/domain/Product.kt
@@ -1,3 +1,3 @@
 package com.prameprimo.shop.products.domain
 
-data class Product(val id: ProductId = ProductId(""), val name: ProductName = ProductName(""))
+data class Product(val id: ProductId = ProductId(), val name: ProductName = ProductName())
